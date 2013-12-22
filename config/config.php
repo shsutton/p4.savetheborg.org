5f343daad72ede94311af8f910857ca18d43fd30
shsutton <shsutton@gmail.com>
2013-12-22 12:01:16 -0500

    fourth commit

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p4_savetheborg_org'); 
+	define('DB_NAME', 'savetheb_p4_savetheborg_org'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
