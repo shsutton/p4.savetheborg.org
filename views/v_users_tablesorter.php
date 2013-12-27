1e5bde61eda829c68b441e25a0874851dff0e470
Stu HS <shsutton@gmail.com>
2013-12-27 05:15:14 -0500

    second new

---
+++ views/v_users_tablesorter.php
@@ -0,0 +1,71 @@
+
+ <html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+ 
+  <link rel="stylesheet" 
+	href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/themes/eggplant/jquery-ui.css" 
+	type="text/css" media="all" />
+	
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
+    type="text/javascript"></script>
+	
+  <script
+    src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"
+    type="text/javascript"></script>
+	
+  <script type="text/javascript">
+    $(function(){
+				$('#tabs').tabs();
+    });      
+    </script>
+
+
+
+
+
+  <title>New Hire Info.</title>
+</head>
+
+
+<body>
+  
+  <h1>Dont get fired!</h1>
+  
+  <div id="tabs">
+    <ul>
+      <li><a href="#home">Company</a>
+      </li>
+      <li><a href="#boss">About</a>
+      </li>
+      <li><a href="#places">Expectations</a>
+      </li>
+    </ul>
+    
+    <div id="home">
+      <h2>Work hard</h2>
+      <h3>Get used to it.
+        <br/>You'll be here awhile.</h3>
+      
+      <p> We only hire for the long term!</p>
+      
+    </div>
+    
+<div id="boss">
+      <h2>A word from the Boss.</h2>
+      <p> Do your job and you won't get fired.
+	  </p>
+	  <p>btw I love Ayn Rand and we stole our charter from Wal-mart!	  
+	  </p>
+    </div>
+    
+<div id="places">
+ <h2>Most popular office floor.</h2>
+ <br>First.
+  </div>
+ </div>
+</body>
+</html>
+ 
---
+++ views/v_users_tabsheet.php
@@ -0,0 +1,31 @@
+<h1>Wal-Mart's 10 rules for business (from walmart.com)</h1>
+
+<h3>1. Commit to your business.</h3>
+Believe in it more than anybody else. If you love your work, you'll be out there every day trying to do it the best you possibly can, and pretty soon everybody around will catch the passion from you – like a fever.
+<br>
+<h3>2. Share your profits with all your associates, and treat them as partners.</h3>
+In turn, they will treat you as a partner, and together you will all perform beyond your wildest expectations.
+<br>
+<h3>3. Motivate your partners.</h3>
+Money and ownership alone aren't enough. Set high goals, encourage competition, and then keep score. Don't become too predictable.
+<br>
+<h3>4. Communicate everything you possibly can to your partners.</h3>
+The more they know, the more they'll understand. The more they understand, the more they'll care. Once they care, there's no stopping them.
+<br>
+<h3>5. Appreciate everything your associates do for the business.</h3>
+Nothing else can quite substitute for a few well-chosen, well-timed, sincere words of praise. They're absolutely free – and worth a fortune.
+<br>
+<h3>6. Celebrate your success.</h3>
+Don't take yourself so seriously. Loosen up, and everybody around you will loosen up. Have fun. Show enthusiasm – always. All of this is more important, and more fun, than you think, and it really fools competition.
+<br>
+<h3>7. Listen to everyone in your company.</h3>
+And figure out ways to get them talking. To push responsibility down in your organization, and to force good ideas to bubble up within it, you must listen to what your associates are trying to tell you.
+<br>
+<h3>8. Exceed your customers’ expectations.</h3>
+Give them what they want — and a little more. Make good on all your mistakes, and don't make excuses — apologize. Stand behind everything you do.
+<br>
+<h3>9. Control your expenses better than your competition.</h3>
+This is where you can always find the competitive advantage. You can make a lot of different mistakes and still recover if you run an efficient operation. Or you can be brilliant and still go out of business if you're too inefficient.
+<br>
+<h3>10. Swim upstream.</h3>
+Go the other way. Ignore the conventional wisdom. If everybody else is doing it one way, there's a good chance you can find your niche by going in exactly the opposite direction.
